9e50e355d5dd25ca43404050256b706b0fcdc6ed
Wingo <cmwingo@gmail.com>
2017-02-01 14:38:08 -0800

    Pushing to get more help

--- src/app/message-board/message-board.component.ts
+++ src/app/message-board/message-board.component.ts
@@ -14,48 +14,78 @@ export class MessageBoardComponent implements OnInit {
   public isLoggedIn: boolean;
   users: FirebaseListObservable<any[]>;
   currentUser;
-  currentUserEmail: string = "cmwingo@gmail.com";
+  currentUserEmail: string = "";
+  authSubscription;
   constructor(private userService: UserService, private router: Router) {
     this.userService.checkAuth().subscribe(
-      (authResponse) => {
+      (authResponse) =>  {
         if(authResponse == null) {
           console.log("Not logged in");
           this.isLoggedIn = false;
         } else {
           console.log("Successfully logged in");
+          if(authResponse != undefined) {
+            console.log("Defined");
+          }
           console.log(authResponse);
+          this.currentUser = "Test";
+          console.log(this.currentUser + "authResponse: " + authResponse);
           this.currentUser = authResponse;
+          this.currentUserEmail = authResponse.google.email;
           // let newUser: User = new User(authResponse.google.displayName, authResponse.google.email);
           // console.log("Welcome" + this.currentUser.name + this.currentUser.email);
           // this.currentUser = newUser;
           this.isLoggedIn = true;
+          debugger;
           // if(this.isLoggedIn) {
           //   this.currentUser.name = authResponse.google.displayName;
           //   this.currentUser.email = authResponse.google.email;
           //   console.log("Current User" + this.currentUser.name + this.currentUser.email);
           // }
         }
+      },
+      (err) => {
+        console.log("Recieved error: ", err);
+      },
+      (data) => {
+        console.log("Completed");
       }
     );
-    this.userService.getUserByEmail(this.currentUserEmail).subscribe(
-      (emailResponse) => {
-        if(this.isLoggedIn == true) {
-          this.currentUser = emailResponse;
-          console.log(emailResponse);
-        }
-      }
-    );
+    // this.userService.getUserByEmail(this.currentUserEmail).subscribe(
+    //   (emailResponse) => {
+    //     if(this.isLoggedIn == true) {
+    //       // this.currentUserEmail = emailResponse["object"].email;
+    //       console.log("Email subscribe " + emailResponse);
+    //     }
+    //   }
+    // );
+    // if(this.currentUser) {
+    //   this.currentUser.subscribe(
+    //     (currentUserData) => {
+    //       this.currentUserEmail = currentUserData.email;
+    //       console.log("Changing current email to: " + currentUserData.email);
+    //     }
+    //   )
+    // }
   }
 
   ngOnInit() {
     this.users = this.userService.getUsers();
-    if(this.currentUser) {
-      this.currentUser.subscribe(
-        (currentUserData) => {
-          this.currentUserEmail = currentUserData.email;
-        }
-      )
+    if(this.currentUser != undefined) {
+      this.currentUserEmail = this.currentUser.email;
     }
+  }
+
+  ngDoCheck() {
+    if(this.currentUser != undefined && this.currentUser.google.displayName != undefined) {
+      console.log("Current User: " + this.currentUser.google.displayName);
+      this.currentUserEmail = this.currentUser.google.email;
+      console.log("Do Check changine email to: " + this.currentUserEmail);
+    }
+  }
+
+  ngOnDestroy() {
+    console.log("On Destroy:" + this.currentUserEmail);
   }
 
   login() {
@@ -66,6 +96,7 @@ export class MessageBoardComponent implements OnInit {
 
   logout() {
     console.log("Logging Out");
+    this.currentUser = null;
     this.userService.logout();
   }
 
