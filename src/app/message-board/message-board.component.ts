2dab69537279c8d323f77cca504d26862514723a
Wingo <cmwingo@gmail.com>
2017-01-31 18:18:12 -0800

    Success!

--- src/app/message-board/message-board.component.ts
+++ src/app/message-board/message-board.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../user.service';
 import { Router } from '@angular/router';
-import { FirebaseObjectObservable } from 'angularfire2';
+import { FirebaseObjectObservable, FirebaseListObservable } from 'angularfire2';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-message-board',
@@ -11,6 +12,9 @@ import { FirebaseObjectObservable } from 'angularfire2';
 })
 export class MessageBoardComponent implements OnInit {
   public isLoggedIn: boolean;
+  users: FirebaseListObservable<any[]>;
+  currentUser;
+  currentUserEmail: string = "cmwingo@gmail.com";
   constructor(private userService: UserService, private router: Router) {
     this.userService.checkAuth().subscribe(
       (authResponse) => {
@@ -19,13 +23,39 @@ export class MessageBoardComponent implements OnInit {
           this.isLoggedIn = false;
         } else {
           console.log("Successfully logged in");
+          console.log(authResponse);
+          this.currentUser = authResponse;
+          // let newUser: User = new User(authResponse.google.displayName, authResponse.google.email);
+          // console.log("Welcome" + this.currentUser.name + this.currentUser.email);
+          // this.currentUser = newUser;
           this.isLoggedIn = true;
+          // if(this.isLoggedIn) {
+          //   this.currentUser.name = authResponse.google.displayName;
+          //   this.currentUser.email = authResponse.google.email;
+          //   console.log("Current User" + this.currentUser.name + this.currentUser.email);
+          // }
+        }
+      }
+    );
+    this.userService.getUserByEmail(this.currentUserEmail).subscribe(
+      (emailResponse) => {
+        if(this.isLoggedIn == true) {
+          this.currentUser = emailResponse;
+          console.log(emailResponse);
         }
       }
     );
   }
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
+    if(this.currentUser) {
+      this.currentUser.subscribe(
+        (currentUserData) => {
+          this.currentUserEmail = currentUserData.email;
+        }
+      )
+    }
   }
 
   login() {
