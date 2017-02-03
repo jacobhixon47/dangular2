b534be45a1475e94c914a8123cd55c46bc8e2846
Jacob Hixon <jacobhixon47@gmail.com>
2017-02-02 20:51:44 -0800

    Delete api-keys.ts

--- src/app/api-keys.ts
+++
@@ -1,7 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyAJKk1bsMIe3PoCX7v8QKVbZ3EEuixUKcU",
-  authDomain: "angular2-help.firebaseapp.com",
-  databaseURL: "https://angular2-help.firebaseio.com",
-  storageBucket: "angular2-help.appspot.com",
-  messagingSenderId: "590095479081"
-}
